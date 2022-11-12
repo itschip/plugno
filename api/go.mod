87c4c80a7ea218981d680704d30d26270019b042
chip <chip@projecterror.dev>
2022-11-12 23:50:12 +0100

    feat(app): new monorepo setup

---
+++ api/go.mod
@@ -0,0 +1,3 @@
+module plugno-api
+
+go 1.18
---
+++ frontend/src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { pocketBaseClient } from "./lib/pocketbase";
+
+    const handleAuth = async () => {
+        await pocketBaseClient.users.create({
+            email: "test@user.com",
+            password: "12345678",
+            passwordConfirm: "12345678"
+        })
+    }
+</script>
+
+<main>
+    <button on:click={handleAuth}>Create user</button>
+</main>
+
+<style>
+</style>
