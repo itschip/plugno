d0a7c48c82286cbb8f50e53aad241b75fb2cd80e
itschip <chip@projecterror.dev>
2022-12-05 21:55:29 +0100

    feat(packages/ui): initial ui pkg stuff

--- apps/web/app/app/profile/general/page.tsx
+++ apps/web/app/app/profile/general/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@plugs/ui';
+
 export default function ProfileGeneralPage() {
 	return (
 		<div>
-			<h1 className="text-white font-extrabold text-3xl">Generelt</h1>
+			<h1 className="mb-4 text-white font-extrabold text-3xl">Generelt</h1>
+			<Button variant="primary">Hello</Button>
 		</div>
 	);
 }
---
+++ packages/ui/src/Button.tsx
@@ -0,0 +1,39 @@
+import React, { ButtonHTMLAttributes } from "react";
+import { cva, cx, type VariantProps } from "class-variance-authority";
+
+export type ButtonProps = VariantProps<typeof classes> &
+  ButtonHTMLAttributes<HTMLButtonElement> & {
+    children: React.ReactNode;
+  };
+
+const classes = cva("text-white py-2 px-3 rounded-md", {
+  variants: {
+    size: {
+      sm: "py-1 px-2",
+      md: "py-2 px-4",
+    },
+    variant: {
+      primary: [
+        "bg-rose-500/60 border border-rose-600/60 hover:bg-rose-600/60",
+      ],
+      secondary: ["bg-neutral-800 border border-neutral-700"],
+    },
+  },
+  defaultVariants: {
+    size: "md",
+    variant: "primary",
+  },
+});
+
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  className,
+  ...props
+}) => {
+  className = cx(classes(props), className);
+  return (
+    <button {...props} className={className}>
+      {children}
+    </button>
+  );
+};
